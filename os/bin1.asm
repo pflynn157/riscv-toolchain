1a5aee3e76fa8a9c072c15ebd21add3f853097e1
Patrick Flynn <pflynn157@gmail.com>
2022-11-11 13:34:53 -0500

    First example of working inner function call

--- os/bin1.asm
+++ os/bin1.asm
@@ -3,15 +3,18 @@ test1:
     addi sp, sp, 8
     sw ra, 0(sp)
     
-    ;addi x18, x0, 7
-    
+    ; println location
     add x14, x0, x1
-    add x15, x0, x2
-    add x16, x14, x15
-
-    addi x19, x0, 823
-    add x1, x0, x19
-    jalr ra, x16, 0
+    
+    ; string location of new base pointer
+    addi x15, bp, STR1
+    addi x15, x15, 8
+    
+    sw bp, 4(sp)
+    add bp, x0, x2
+    add x1, x0, x15
+    jalr ra, x14, 0
+    lw bp, 4(sp)
     
     lw ra, 0(sp)
     addi sp, sp, -8
--- os/kernel.asm
+++ os/kernel.asm
@@ -15,16 +15,20 @@ kmain:
     
     addi x20, x0, 60
     
-    addi x1, x0, MSG1
+    ;addi x1, x0, MSG1
+    addi x1, bp, MSG1
     jalr ra, bp, println
     
-    addi x1, x0, STR1
+    ;addi x1, x0, STR1
+    addi x1, bp, STR1
     jalr ra, bp, println
     
-    add x1, x0, bp
-    addi x2, x0, println
-    addi x3, bp, BOOT_END
-    jalr ra, bp, BOOT_END
+    addi x1, bp, println
+    sw bp, 4(sp)
+    add x2, x0, bp
+    addi bp, bp, BOOT_END
+    jalr ra, x2, BOOT_END
+    lw bp, 4(sp)
     
     lw ra, 0(sp)
     addi sp, sp, -8
@@ -35,7 +39,8 @@ println:
     sw ra, 0(sp)
     
     ; Setup the base pointer and get string length
-    add x3, bp, x1
+    ;add x3, bp, x1
+    add x3, x0, x1
     sw x3, 4(sp)
     jalr ra, bp, strlen
     add x2, x0, x1
@@ -72,7 +77,7 @@ strlen:
     addi sp, sp, 8
     sw ra, 0(sp)
     
-    add x1, x1, bp
+    ;add x1, x1, bp
     addi x2, x0, 0
     lb x3, 0(x1)
     LOOP_CMP1:
@@ -85,6 +90,8 @@ strlen:
     DONE1:
     
     add x1, x0, x2
+    ;add x23, x0, x1
+    ;add x23, x0, x1
     
     lw ra, 0(sp)
     addi sp, sp, -8
