92491c4904de631ba28492ab8cec34729951ba9a
Patrick Flynn <pflynn157@gmail.com>
2022-11-11 14:40:47 -0500

    Slight reorganization

--- os1.sh
+++ os1.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-as/src/rvas ../os/kernel.asm kernel.bin
-as/src/rvas ../os/bin1.asm bin1.bin
+as/src/rvas ../os/os1/kernel.asm kernel.bin
+as/src/rvas ../os/os1/bin1.asm bin1.bin
 rm sata0.bin
 
 cat kernel.bin bin1.bin > complete.bin
 
 dd if=/dev/zero of=sata0.bin bs=512 count=2048
 dd if=complete.bin of=sata0.bin status=none conv=notrunc
+
+as/src/rvas ../os/os1/boot.asm boot.bin
+sim/sim
 
--- run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-as/src/rvas ../os/boot.asm boot.bin
-sim/sim
-
